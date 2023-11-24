2dca17eb7ed77fe6d9ac756ac65862586b9bc785
wu <timmy61501@gmail.com>
2023-11-24 11:02:47 +0800

    繼承特性

--- index.php
+++ index.php
@@ -19,12 +19,27 @@ class Animal{
 }
 
 // 實例化 instant
-$animal=new Animal('漢堡');
+// $animal=new Animal('漢堡');
 
-echo '顯示名稱:' .$animal->getName();
+// echo '顯示名稱:' .$animal->getName();
+// echo "<br>";
+// $animal->setName('漢堡');
+// // -> 物件顯示
+// echo $animal->name;
+// $animal->setName('漢堡');
+// echo '顯示名稱:' .$animal->getName();
+
+// 繼承特性
+class Dog extends Animal{
+    function sit(){
+        echo "趴下";
+    }
+}
+
+$dog=new Dog('來福');
+echo $dog->getName();
 echo "<br>";
-$animal->setName('漢堡');
-// -> 物件顯示
-echo $animal->name;
-$animal->setName('漢堡');
-echo '顯示名稱:' .$animal->getName();
+echo $dog->setName('來西');
+echo $dog->getName();
+$dog->sit();
+
